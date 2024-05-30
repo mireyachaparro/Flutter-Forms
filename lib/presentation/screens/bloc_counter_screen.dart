2686f5eba28e3d96322d9ab4d262ad0944f82534
MIREYA CHAPARRO NIETO <mireya.chaparro@ahorramas.com>
2024-05-30 15:32:28 +0200

    Add bloc counter screen

---
+++ lib/presentation/screens/bloc_counter_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class BlocCounterScreen extends StatelessWidget {
+  const BlocCounterScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('BlocCounter'),
+        actions: [
+          IconButton(onPressed: () => {}, icon: Icon(Icons.refresh_outlined))
+        ],
+      ),
+      body: Center(
+        child: const Text('Counter value: xxx'),
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            heroTag: '1', 
+            child: const Text('+3'), 
+            onPressed: () => {}
+          ),
+          const SizedBox(height: 15,),
+          FloatingActionButton(
+            heroTag: '2', 
+            child: const Text('+2'), 
+            onPressed: () => {}
+          ),
+          const SizedBox(height: 15,),
+          FloatingActionButton(
+            heroTag: '3', 
+            child: const Text('+1'), 
+            onPressed: () => {}
+          ),
+        ],
+      ),
+    );
+  }
+}
