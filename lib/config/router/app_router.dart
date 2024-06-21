cf9b9e70507ed8e7b150dd47323d40934c64e263
MIREYA CHAPARRO NIETO <mireya.chaparro@ahorramas.com>
2024-06-21 11:28:17 +0200

    create register screen

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -17,4 +17,9 @@ final appRouter = GoRouter(
       path: '/counter-bloc', 
       builder: (context, state) => const BlocCounterScreen()
     ),
+
+    GoRoute(
+      path: '/new-user', 
+      builder: (context, state) => const RegisterScreen()
+    ),
 ]);
