f9ae0f718cf340b02a8ec5a2aac2cbecf70483a5
MIREYA CHAPARRO NIETO <mireya.chaparro@ahorramas.com>
2024-06-24 10:10:45 +0200

    validators in register form

--- lib/presentation/screens/register_screen.dart
+++ lib/presentation/screens/register_screen.dart
@@ -43,35 +43,77 @@ class _RegisterView extends StatelessWidget {
   }
 }
 
-class _RegisterForm extends StatelessWidget {
+class _RegisterForm extends StatefulWidget {
   const _RegisterForm();
+
+  @override
+  State<_RegisterForm> createState() => _RegisterFormState();
+}
+
+class _RegisterFormState extends State<_RegisterForm> {
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  String username = '';
+  String email = '';
+  String password = '';
 
   @override
   Widget build(BuildContext context) {
     return Form(
+      key: _formKey,
       child: Column(
         children: [
-          const CustomTextFormField(
+          CustomTextFormField(
             label: 'Nombre de usuario',
+            onChanged: (value) => username = value,
+            validator: (value){
+              if(value == null || value.isEmpty) return 'Campo necesario';
+              if(value.trim().isEmpty) return 'Campo requerido';
+              if(value.length < 6) return 'Minimo 6 caracteres';
+              return null;
+            },
           ),
 
           const SizedBox(height: 20),
 
-          const CustomTextFormField(
+          CustomTextFormField(
             label: 'Correo electrónico',
+            onChanged: (value) => email = value,
+            validator: (value){
+              if(value == null || value.isEmpty) return 'Campo necesario';
+              if(value.trim().isEmpty) return 'Campo requerido';
+              
+              final emailRegExp = RegExp(
+                r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$',
+              );
+
+              if(!emailRegExp.hasMatch(value)) return 'No tiene formato de correo';
+              return null;
+            },
           ),
 
           const SizedBox(height: 20),
 
-          const CustomTextFormField(
+          CustomTextFormField(
             label: 'Contraseña',
-            obscureText: true
+            obscureText: true,
+            onChanged: (value) => password = value,
+            validator: (value){
+              if(value == null || value.isEmpty) return 'Campo necesario';
+              if(value.trim().isEmpty) return 'Campo requerido';
+              if(value.length < 6) return 'Minimo 6 caracteres';
+              return null;
+            },
           ),
 
           const SizedBox(height: 20),
           
           FilledButton.tonalIcon(
-                onPressed: (){}, 
+                onPressed: (){
+                  final isValid = _formKey.currentState!.validate();
+                  if(!isValid) return;
+                  print('$username, $email, $password');
+                }, 
                 icon: const Icon(Icons.save),
                 label: const Text('Crear usuario')
               ),
